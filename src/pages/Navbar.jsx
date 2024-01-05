2581a8bb581e918df6ad724dca27fa9a5f3dd185
Shivankit Agarwal <shivankit1210@gmail.com>
2024-01-05 22:43:35 +0530

    Updation in Navbar file

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,6 +3,11 @@ import React from "react";
 const Navbar = () =>{
     return(
         <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Contact us</li>
+                <li>About us</li>
+            </ul>
            
         </div>
     )
